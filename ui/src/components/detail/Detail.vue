6662f71146e9683e8ef0bdeb7a866d2edd28a75b
Jack Mordaunt <jackmordaunt@gmail.com>
2018-08-17 14:52:34 +1200

    [~] Refactor away from bootstrap list-group.

--- ui/src/components/detail/Detail.vue
+++ ui/src/components/detail/Detail.vue
@@ -1,85 +1,71 @@
 <template>
-    <b-container fluid>
-        <b-row id="show-description">
-            <b-col>
-                <b-container fluid>
-                    <b-row>
-                        <div class="background"
-                             :style="backgroundImage">
+    <div id="detail">
+        <section id="show-description">
+            <div class="background"
+                 :style="backgroundImage">
+            </div>
+            <div class="cover-img">
+                <img :src="cover"/>
+            </div>
+            <div class="meta">
+                <h1>{{name}}</h1>
+                <div class="infos">
+                    <p>{{year}}</p>
+                    <p>{{runtime}}</p>
+                    <p>{{status}}</p>
+                    <p>{{genre}}</p>
+                    <p>{{rating}}/10</p>
+                </div>
+                <p class="synopsis">
+                    {{synopsis}}
+                </p>
+            </div>
+        </section>
+        <section id="episode-browser">
+            <div class="season-list">
+                <h2>Seasons</h2>
+                <ul class="scrollable list-group">
+                    <li v-for="(season, ii) in seasons"
+                        :key="ii"
+                        :active="isActiveSeason(ii)"
+                        @click="setActiveSeason(ii)"
+                        class="list-group-item"
+                    >
+                        <div class="season">
+                            Season {{ii+1}}
                         </div>
-                        <b-col sm="4" md="3" lg="2" >
-                            <div class="cover-img">
-                                <img :src="cover"/>
-                            </div>
-                        </b-col>
-                        <b-col sm="7" md="8" lg="9" >
-                            <div class="meta">
-                                <h1>{{name}}</h1>
-                                <div class="infos">
-                                    <p>{{year}}</p>
-                                    <p>{{runtime}}</p>
-                                    <p>{{status}}</p>
-                                    <p>{{genre}}</p>
-                                    <p>{{rating}}/10</p>
-                                </div>
-                                <p class="synopsis">
-                                    {{synopsis}}
-                                </p>
-                            </div>
-                        </b-col>
-                    </b-row>
-                </b-container>
-            </b-col>
-        </b-row>
-        <b-row id="episode-browser">
-            <b-col sm="3" md="3" lg="2">
-                <div class="season-list">
-                    <h2>Seasons</h2>
-                    <b-list-group flush>
-                        <b-list-group-item v-for="(season, ii) in seasons"
-                                           :key="ii"
-                                           :active="isActiveSeason(ii)"
-                                           @click="setActiveSeason(ii)">
-                            <div class="season">
-                                Season {{ii+1}}
-                            </div>
-                        </b-list-group-item>
-                    </b-list-group>
+                    </li>
+                </ul>
+            </div>
+            <div class="episode-list">
+                <h2>Episodes</h2>
+                <ul class="scrollable list-group">
+                    <li v-for="(ep, kk) in season.episodes"
+                        :key="ep.name"
+                        :active="isActiveEpisode(kk)"
+                        @click="setActiveEpisode(kk)"
+                        class="list-group-item"
+                    >
+                        <div class="episode">
+                            Episode {{kk+1}}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="episode-description">
+                <h2>{{episode.name}}</h2>
+                <div class="number">
+                    Season {{activeSeason+1}}, Episode {{activeEpisode + 1}}
                 </div>
-            </b-col>
-            <b-col sm="4" md="5" lg="6">
-                <div class="episode-list">
-                    <h2>Episodes</h2>
-                    <b-list-group flush>
-                        <b-list-group-item v-for="(ep, kk) in season.episodes"
-                                           :key="ep.name"
-                                           :active="isActiveEpisode(kk)"
-                                           @click="setActiveEpisode(kk)">
-                            <div class="episode">
-                                Episode {{kk+1}}
-                            </div>
-                        </b-list-group-item>
-                    </b-list-group>
+                <div class="date">
+                    Aired Date: {{episode.releaseDate.toLocaleString()}}
                 </div>
-            </b-col>
-            <b-col sm="5" md="4">
-                <div class="episode-description">
-                    <h2>{{episode.name}}</h2>
-                    <div class="number">
-                        Season {{activeSeason+1}}, Episode {{activeEpisode + 1}}
-                    </div>
-                    <div class="date">
-                        Aired Date: {{episode.releaseDate.toLocaleString()}}
-                    </div>
-                    <p>{{episode.synopsis}}</p>
-                    <div class="controls">
-                        <div id="health"></div>
-                        <div id="watch"></div>
-                    </div>
-                </div>
-            </b-col>
-        </b-row>
-    </b-container>
+                <p class="episode-synopsis">
+                    {{episode.synopsis}}
+                </p>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
@@ -134,93 +120,79 @@ export default {
 </script>
 
 <style scoped>
-
-#show-description {
-    padding: 0;
-    overflow: hidden;
-    height: 220px;
-}
-
-#episode-browser {
-    height: calc(100vh - 220px);
-    width: 100vw;
-    overflow: auto;
-}
-
-.season-list {
-    height: 100%;
-    padding-top: 20px;
-}
-
-.episode-list {
-    height: 100%;
-    padding-top: 20px;
-}
-
-.episode-description {
-    height: 100%;
-    padding-top: 20px;
-}
-
-.episode-description > p {
-    margin-top: 10px;
-    color: white;
-}
-
-.episode-description .controls {
-    position: relative;
-    height: 100%;
-    width: 100%;
-}
-
-#watch {
-    position: absolute;
-    background: green;
-    bottom: 10px;
-    left: 10px;
-    width: 100px;
-    height: 100px;
-}
-
-.list-group {
-    overflow: auto;
+h1 {
+    font-size: 3em;
+    margin: 0;
 }
 
 h2 {
     font-size: 2em;
     color: white;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 }
 
-.list-group-item {
-    border: none;
-    color: white;
+#detail {
+    height: 100%;
+    width: 100%;
+}
+
+#show-description {
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+    height: 220px;
+    padding: 10px;
+}
+
+#episode-browser {
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    max-height: calc(100vh - 220px);
+    width: 100vw;
     background-color: black;
-    margin-top: 1px;
-    margin-bottom: 1px;
+    padding: 10px;
 }
 
-.list-group-item:nth-child(odd) {
-    background-color: #131313;
+#episode-browser > div {
+    margin: 5px;
 }
 
-.list-group-item:hover {
-    border: none;
-    transition: 0.1s ease;
-    background-color: #232323;
+#episode-browser > div:nth-child(1) {
+    flex-grow: 1;
 }
 
-.list-group-item.active {
-    border-left: 2px solid yellow;
-    background-color: #232323;
+#episode-browser > div:nth-child(2) {
+    flex-grow: 2;
+}
+
+#episode-browser > div:nth-child(3) {
+    flex-grow: 1;
+}
+
+.episode-description {
+    max-width: 33vw;
+    color: white;
+}
+
+.episode-synopsis {
+    margin-top: 5px;
+    padding-right: 5px;
+    height: calc(100% - 90px);
+    overflow-y: auto;
+}
+
+.scrollable {
+    height: calc(100% - 40px);
+    overflow-y: auto;
 }
 
 .cover-img {
-    margin-top: 10px;
-    margin-bottom: 10px;
+    flex-grow: 1;
     height: 200px;
-    width: 130px;
-    margin-left: auto;
-    margin-right: auto;
+    min-width: 134px;
+    max-width: 134px;
 }
 
 .cover-img > img {
@@ -231,12 +203,22 @@ h2 {
 }
 
 .meta {
-    color: white;
     height: 100%;
+    flex-grow: 2;
+    min-width: 200px;
+    color: white;
+    padding-left: 10px;
+}
+
+.synopsis {
+    margin-top: 10px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    max-height: 90px;
 }
 
 .infos {
-    overflow: auto;
+    overflow-x: auto;
     white-space: nowrap;
     text-overflow: ellipsis;
 }
@@ -262,15 +244,32 @@ h2 {
     top: 0;
     left: 0;
     right: 0;
-    bottom: 0;
     width: 100vw;
-    height: 100%;
-    min-height: 200px;
+    height: 220px;
     filter: blur(60px) brightness(0.7);
+    z-index: -1;
 }
 
-.synopsis {
-    padding-top: 10px;
-    overflow: auto;
+.list-group-item {
+    border-radius: 0px !important;
+    background-color: black;
+    color: white;
+    margin-top: 1px;
+    margin-bottom: 1px;
+}
+
+.list-group-item:nth-child(odd) {
+    background-color: #131313;
+}
+
+.list-group-item:hover {
+    transition: 0.1s ease;
+    background-color: #232323;
+    border-color: #232323;
+}
+
+.list-group-item[active] {
+    box-shadow: 0;
+    border-left: 1px solid  yellow;
 }
 </style>
