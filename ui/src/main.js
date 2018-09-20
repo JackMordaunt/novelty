a808aee9dadada5da2936502c5bee7eb3b9b15a7
Jack Mordaunt <jackmordaunt@gmail.com>
2018-09-20 13:40:27 +1200

    [+] Register websocket connection and make it globally accessible.

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,7 +1,8 @@
-/* eslint-disable */
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
+import Settings from "./settings"
+import WS from "./services/Websocket"
 
 // import 'bootstrap/dist/css/bootstrap.css'
 import './styles/bootstrap.min.css'
@@ -14,28 +15,8 @@ Vue.use(BootstrapVue)
 Vue.component("icon", Icon)
 Vue.config.productionTip = false
 
+Vue.prototype.$ws = new WS(`ws://${Settings.server_address}/ws`)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-import axios from "axios"
-
-let http = axios.create({
-  baseURL: "http://127.0.0.1",
-  port: 9090,
-})
-
-axios.post("/open", {
-  "torrent_path": "magnet:?xt=urn:btih:674D163D2184353CE21F3DE5196B0A6D7C2F9FC2&dn=bbb_sunflower_1080p_60fps_stereo_abl.mp4&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.publicbt.com%3a80%2fannounce&ws=http%3a%2f%2fdistribution.bbb3d.renderfarming.net%2fvideo%2fmp4%2fbbb_sunflower_1080p_60fps_stereo_abl.mp4",
-})
-
-import Socket from "simple-websocket"
-
-let ws = new Socket("ws://127.0.0.1:9090/1/stats")
-ws.on("data", (data) => {
-  console.log(data)
-})
-
-ws.on("error", (err) => {
-  console.log(err)
-})
